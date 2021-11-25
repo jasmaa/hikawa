43296d9f9ec3a02e9b4fb22d0fc11abc18c63588
jasmaa <jasonmaa3955@gmail.com>
2021-11-25 09:10:36 -0500

    Add link navigation

--- pkg/ui/main.go
+++ pkg/ui/main.go
@@ -2,6 +2,8 @@ package ui
 
 import (
 	"fmt"
+	"net/url"
+	"path"
 	"time"
 
 	"github.com/godot-go/godot-go/pkg/gdnative"
@@ -14,7 +16,8 @@ type Main struct {
 	gdnative.NodeImpl
 	gdnative.UserDataIdentifiableImpl
 
-	client gemini.Client
+	client     gemini.Client
+	currentUrl string
 }
 
 func (p *Main) ClassName() string {
@@ -36,11 +39,44 @@ func (p *Main) Ready() {
 }
 
 func (p *Main) OnSearchBarTextEntered(newText string) {
-	p.navigatePage()
+	searchBar := gdnative.NewLineEditWithOwner(p.GetNode(gdnative.NewNodePath("SearchBar")).GetOwnerObject())
+	newUrl := p.navigatePage(searchBar.GetText())
+	p.currentUrl = newUrl
+	searchBar.SetText(newUrl)
+	searchBar.ReleaseFocus()
 }
 
 func (p *Main) OnSearchButtonPressed() {
-	p.navigatePage()
+	searchBar := gdnative.NewLineEditWithOwner(p.GetNode(gdnative.NewNodePath("SearchBar")).GetOwnerObject())
+	newUrl := p.navigatePage(searchBar.GetText())
+	p.currentUrl = newUrl
+	searchBar.SetText(newUrl)
+}
+
+func (p *Main) OnContentMetaClicked(meta string) {
+	searchBar := gdnative.NewLineEditWithOwner(p.GetNode(gdnative.NewNodePath("SearchBar")).GetOwnerObject())
+
+	target, err := url.Parse(meta)
+	if err != nil {
+		return
+	}
+	var targetUrl string
+	if len(target.Scheme) == 0 {
+		u, _ := url.Parse(p.currentUrl)
+		u.Path = path.Join(u.Path, meta)
+		targetUrl = u.String()
+	} else {
+		switch target.Scheme {
+		case "gemini":
+			targetUrl = meta
+		default:
+			return
+		}
+	}
+
+	newUrl := p.navigatePage(targetUrl)
+	p.currentUrl = newUrl
+	searchBar.SetText(newUrl)
 }
 
 func (p *Main) OnClassRegistered(e gdnative.ClassRegisteredEvent) {
@@ -48,6 +84,7 @@ func (p *Main) OnClassRegistered(e gdnative.ClassRegisteredEvent) {
 	e.RegisterMethod("_ready", "Ready")
 	e.RegisterMethod("_on_SearchButton_pressed", "OnSearchButtonPressed")
 	e.RegisterMethod("_on_SearchBar_text_entered", "OnSearchBarTextEntered")
+	e.RegisterMethod("_on_Content_meta_clicked", "OnContentMetaClicked")
 }
 
 func NewMainWithOwner(owner *gdnative.GodotObject) Main {
@@ -61,13 +98,14 @@ func init() {
 	})
 }
 
-func (p *Main) navigatePage() {
-	searchBar := gdnative.NewLineEditWithOwner(p.GetNode(gdnative.NewNodePath("SearchBar")).GetOwnerObject())
+func (p *Main) navigatePage(url string) string {
 	content := gdnative.NewRichTextLabelWithOwner(p.GetNode(gdnative.NewNodePath("Content")).GetOwnerObject())
+	defer content.ScrollToLine(0)
 
-	clientResp, err := p.client.NavigatePage(searchBar.GetText())
+	clientResp, err := p.client.NavigatePage(url)
 	if err != nil {
 		content.SetBbcode(err.Error())
+		return url
 	} else {
 		if clientResp.Response.Header.Status == gemini.STATUS_SUCCESS {
 			if clientResp.Response.Header.Meta == "text/gemini" {
@@ -79,6 +117,6 @@ func (p *Main) navigatePage() {
 		} else {
 			content.SetBbcode(fmt.Sprintf("[%d] %s", clientResp.Response.Header.Status, clientResp.Response.Header.Meta))
 		}
-		searchBar.SetText(clientResp.Url)
+		return clientResp.Url
 	}
 }
